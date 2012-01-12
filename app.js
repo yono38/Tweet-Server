7b17f5d2374bf47685fe3d564ebb1f11e73cf542
Jason S <yono38@gmail.com>
2012-01-11 23:37:22 -0500

    added echonest API support (version 1.2)

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var app = require('express').createServer(),
-	 twitter = require('ntwitter'),
-   io = require('socket.io').listen(app);
+	 twitter = require('ntwitter'), http= require('http'),
+   io = require('socket.io').listen(app),
+   config = require('./lib/config');
   
 var twit = new twitter({
-   consumer_key: 'TPI5elAnP0abdruouDCO7w',
-   consumer_secret: 'g2EMLDqsX2yrJE3lx496UkuD2W9AtV0BaipjlYzw2o',
-   access_token_key: '45428056-FV7jnxzkSL2hdn9Vk6f73b2Xc2Btr15tli10By1jo',
-   access_token_secret: 'nUNYO4uhQCU5v9WQ48VjLESSvyvohomtz46aKZfH8'
+   consumer_key: config.tw_consumer_key,
+   consumer_secret: config.tw_consumer_secret,
+   access_token_key: config.tw_access_token_key,
+   access_token_secret: config.tw_access_token_secret
  });
+
+
 
 app.listen(8888);
 
@@ -37,8 +40,14 @@ app.get('/stream/:term', function(req, res) {
 io.sockets.on('connection', function (socket) {
 	// the custom 'page_load' event reads in the search term from the client's URL
   socket.on('page_load', function(url_data){
-  console.log("PAGE LOADED ++++++++++++++++++++++++++++++++++++");
-  console.log(url_data.topic);
+  console.log("PAGE LOADED +++++++++++++++++++++++++++++++++++ "+url_data.topic);
+  
+  if (config.use_echonest){
+  	var topic_term = url_data.topic.replace(' ', '+');
+  	console.log('topic term: '+topic_term);
+  	echonest(topic_term, config, socket);
+  }
+  
   // this opens up a tracking stream on the twitter API
    twit.stream('statuses/filter', {'track': url_data.topic}, function(stream) {
 	   stream.on('data', function (twit_data) {
@@ -48,6 +57,55 @@ io.sockets.on('connection', function (socket) {
    });
  }); 
 });
+
+// echonest: Calls the echonest API to get the verified twitter account of the musician in the search term, if
+// the twitter account exists.
+// Term must be in the form "word" or "word+word+word"
+function echonest(term, config, socket){
+	var options = {
+		host: 'developer.echonest.com',
+		port: 80,
+		path: '/api/v4/artist/profile?api_key='+config.echo_key+'&name='+term+'&bucket=id:twitter&format=json'
+	};
+
+	http.get(options, function(res) {
+		var body = "";
+		console.log('STATUS: ' + res.statusCode);
+		res.on('data', function (chunk) {
+		  body += chunk;
+		});
+		res.on('end', function(){
+		// parse the response when it finishes
+		  body = JSON.parse(body)
+			// if a twitter username is returned
+			if (typeof(body.response.artist.foreign_ids) != "undefined"){
+			//	console.log(body.response.artist.foreign_ids[0].foreign_id);
+				// slice out the username from the json
+				name = body.response.artist.foreign_ids[0].foreign_id.split(':').slice(-1)[0];
+				console.log("twitter username:" + name);
+				twitter_status(name, config, socket);
+			}
+		});
+	});
+}
+
+// twitter_status: pulls in the statuses of the user_name passed into the function
+// default tweet limit is 5, set in the config.js file
+function twitter_status(user_name, config, socket) {
+
+	// I'm fairly certain the count option isn't working
+  var twit_options = {
+  	count: config.seed_tweet_limit,
+  	screen_name: user_name,
+  	trim_user: true
+  }
+  twit.get('/statuses/user_timeline.json', twit_options, function(stuff, data){
+	if (typeof(socket) != undefined){
+	  socket.emit('artist_tweet', {artist_data: data});
+	}
+	else console.log('Socket error!');
+  });
+}
 
     
     
--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-function app(app_term) {
+function app(app_term, echonest) {
 	var socket = io.connect('http://localhost/'),
 		tweet_count = 0, tweet_lim = $('#tweet_limit').val();	
 	$(document).ready(function() {
@@ -18,11 +18,19 @@ function app(app_term) {
 		// limiting function, removes first tweet from list
 		// when new tweet added
 		if(tweet_count>tweet_lim) {
-			$("#tweets li:first").remove();
+			$("#tweets li.normal:first").remove();
 			tweet_count--;
 		}									
 		console.log(data); 
-		$('#tweets').append('<li>'+data.data.text+'</li>');												
-	});		
+		$('#tweets').append('<li class="normal">'+data.data.text+'</li>');												
+	});
+	socket.on('artist_tweet', function(data) {
+		console.log('artist tweets received!');
+	  console.log(data);
+	  $('#artist_tweet_box').show();
+	  $.each(data.artist_data, function(k,val){
+	  	$('#artist_tweets').append('<li class="artist_tweet_text">'+val.text+'</li>');			
+	  });
+  });
 }
 
